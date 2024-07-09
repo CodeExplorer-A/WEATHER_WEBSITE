7d1f49e4aa757d89fe3ec0f2080ebb0c5e5bff65
Anusha <anushadasari578@gmail.com>
2024-07-10 00:23:39 +0530

    Update script.js

--- script.js
+++ script.js
@@ -13,7 +13,7 @@ const weather_body = document.querySelector('.weather-body');
 
 
 async function checkWeather(city) {
-    const api_key = "a";
+    const api_key = "Enter Your API Key here !!!!!!!";
     const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${api_key}`;
 
     const weather_data = await fetch(`${url}`).then(response => response.json());
